3f1d239aaf018532df665c616fadc48c12436243
techprograms <39184153+techprograms@users.noreply.github.com>
2022-08-13 23:53:18 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
       <h1>This is India</h1>
       <h2>This is Uttar Pradesh</h2>
+      <h3>This is Jaunpur</h3>
       <Home />
         <h1>Welcome in React World!!</h1>
       </header>
