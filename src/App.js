60bbb2285d83583377df57837c0508863eaf175f
techprograms <tech.gpr@gmail.com>
2022-08-14 00:00:16 +0530

    added some lines of code

--- src/App.js
+++ src/App.js
@@ -7,7 +7,11 @@ function App() {
       <header className="App-header">
       <h1>This is India</h1>
       <h2>This is Uttar Pradesh</h2>
+<<<<<<< Updated upstream
       <h3>This is Jaunpur</h3>
+=======
+      <h4>Lest discuss</h4>
+>>>>>>> Stashed changes
       <Home />
         <h1>Welcome in React World!!</h1>
       </header>
