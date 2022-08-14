2fefd7d0d8e9248d6b7b56f100c43ff610a7db11
techprograms <39184153+techprograms@users.noreply.github.com>
2022-08-14 17:03:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ function App() {
       <h2>This is Uttar Pradesh</h2>
       <h3>This is Jaunpur</h3>
       <h4>Lest discuss</h4>
+      <h5>This is last update</h5>
       <Home />
         <h1>Welcome in React World!!</h1>
       </header>
